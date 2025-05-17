63b26fc9570e5cdd30c32ac911ef0f665cdf2a1f
tkg-reis <oorgns1uw5sk3ae@gmail.com>
2025-05-17 18:04:00 +0900

    insert画面のuiの変更とinsertロジックの追加

--- src/app/insert/page.tsx
+++ src/app/insert/page.tsx
@@ -14,8 +14,15 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { useRouter } from "next/navigation";
 
-// zodの復習
 const formSchema = z.object({
   company_name: z
     .string()
@@ -25,22 +32,49 @@ const formSchema = z.object({
     .max(20),
   url: z
     .string()
-    .min(2, {
-      message: "URLは2文字以上50文字以下にしてください",
+    .url({
+      message : "有効なURLを入力してください"
+    })
+    .min(10, {
+      message: "URLは10文字以上50文字以下にしてください",
     })
     .max(50),
-});
+  execution_time : z
+    .string()
+}).required();
 
 const Insert = () => {
+  const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       url: "",
     },
   });
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+  const onSubmit = async(values: z.infer<typeof formSchema>) => {
+    try {
+          const res = await fetch('/api/insert', {
+            method : 'POST',
+            headers : {
+              'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify(values)
+          });
+
+          if(!res.ok) {
+            alert(`insert failed`);
+            return;
+          } else {
+            alert('insert successful');
+            router.push('/');
+            return;
+          }    
+        } catch (error) {
+          alert(`insert failed ${error}`);
+          return;
+        }
   }
+
   return (
     <>
       <Form {...form}>
@@ -52,9 +86,9 @@ const Insert = () => {
               <FormItem>
                 <FormLabel>url</FormLabel>
                 <FormControl>
-                  <Input placeholder="https://" {...field} />
+                  <Input placeholder="https://amazon.com" {...field} />
                 </FormControl>
-                <FormDescription>you input url.</FormDescription>
+                <FormDescription>you can input url.</FormDescription>
                 <FormMessage />
               </FormItem>
             )}
@@ -68,7 +102,35 @@ const Insert = () => {
                 <FormControl>
                   <Input placeholder="amazon" {...field} />
                 </FormControl>
-                <FormDescription>you input url.</FormDescription>
+                <FormDescription>you can input company name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="execution_time"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>execution time</FormLabel>
+                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a http request" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="1">15分ごとにhttpリクエスト</SelectItem>
+                    <SelectItem value="2">30分ごとにhttpリクエスト</SelectItem>
+                    <SelectItem value="3">1時間ごとにhttpリクエスト</SelectItem>
+                    <SelectItem value="4">3時間ごとにhttpリクエスト</SelectItem>
+                    <SelectItem value="5">6時間ごとにhttpリクエスト</SelectItem>
+                    <SelectItem value="6">12時間ごとにhttpリクエスト</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormDescription>
+                  You can select http request time
+                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
