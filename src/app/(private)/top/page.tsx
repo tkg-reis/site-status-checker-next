3ce67fbe6ba029d5fa0532338b5a2684d4f144ea
tkg-reis <oorgns1uw5sk3ae@gmail.com>
2026-01-02 17:43:51 +0900

    topページが描画されるように修正

--- src/app/(private)/top/page.tsx
+++ src/app/(private)/top/page.tsx
@@ -1,59 +1,56 @@
-// 'use client';
-
+'use client';
+import { useEffect, useState } from "react";
+import { siteUrlType } from "../../types/types";
+import SkeltonCard from "@/components/skeltonCard";
+import MappingSiteURL from "@/components/mappingSiteUrl";
 // import { requireAuth } from "@/lib/auth";
 
-// import { useEffect, useState } from "react";
-// import { siteUrlType } from "../../types/types";
-// import SkeltonCard from "@/components/skeltonCard";
-// import MappingSiteURL from "@/components/mappingSiteUrl";
+export default function Top() {
+  const [loading, setLoading] = useState(true);
 
-export default async function Top() {
-  // const [loading, setLoading] = useState(true);
+  const ENDPOINT = "/api/fetchSiteURLs";
 
-  // const ENDPOINT = "/api/fetchSiteURLs";
+  const [urlDatas, setUrlData] = useState<siteUrlType[]>([]);
 
-  // const [urlDatas, setUrlData] = useState<siteUrlType[]>([]);
+  const fetchData = async (): Promise<siteUrlType[] | void> => {
+    try {
 
-  // const fetchData = async (): Promise<siteUrlType[] | void> => {
-  //   try {
-
-  //     const res: siteUrlType[] = await fetch(ENDPOINT, {
-  //       cache: "no-store",
-  //     }).then((res) => res.json());
+      const res: siteUrlType[] = await fetch(ENDPOINT, {
+        cache: "no-store",
+      }).then((res) => res.json());
       
-  //     setUrlData((prev) => [...prev, ...res]);
+      setUrlData((prev) => [...prev, ...res]);
 
-  //   } catch (error) {
-  //     throw new Error(`Failed to fetch data ${error}`);
-  //   }
-  // };
+    } catch (error) {
+      throw new Error(`Failed to fetch data ${error}`);
+    }
+  };
 
-  // useEffect(() => {
-  //   try {
-  //     fetchData();
-  //   } catch (error) {
-  //     throw new Error(`Failed to fetch data ${error}`);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // }, []);
+  useEffect(() => {
+    try {
+      fetchData();
+    } catch (error) {
+      throw new Error(`Failed to fetch data ${error}`);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
 
-  // if (loading) return <SkeltonCard/>;
+  if (loading) return <SkeltonCard/>;
 
-   // 認証チェック
+  // 認証チェック
   // const user = await requireAuth();
   // console.log('ユーザー情報:', { user: user?.email });
 
   return (
-    // <>
-    //   <div className="flex gap-x-8 gap-y-4 flex-wrap">
-    //     {/* {requestUrl} */}
-    //     {urlDatas != null
-    //       ? <MappingSiteURL urlDatas={urlDatas} />
-    //       : "ネットワーク未接続またはDB接続エラーです"}
-    //   </div>
+    <>
+      <div className="flex gap-x-8 gap-y-4 flex-wrap">
+        {/* {requestUrl} */}
+        {urlDatas != null
+          ? <MappingSiteURL urlDatas={urlDatas} />
+          : "ネットワーク未接続またはDB接続エラーです"}
+      </div>
      
-    // </>
-      <h1>hello</h1>
+    </>
   );
 }
